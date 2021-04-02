ca3a194573d30b9dddb71f755eb59c2e3b1b70eb
jbrit <pro.ajibolaojo@gmail.com>
2021-04-02 10:17:37 +0100

    Whitenoise and static files set up

--- pdpolls/settings.py
+++ pdpolls/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -97,7 +98,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Some Development settings
 if DEBUG:
